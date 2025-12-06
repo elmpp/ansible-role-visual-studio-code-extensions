af3b4f7f2bd291d058194468b61585d41438bc89
matt penrice <matthew.penrice@gmail.com>
2025-12-06 15:25:02 +0100

    add support for antigravity and cursor

--- library/visual_studio_code_extensions.py
+++ library/visual_studio_code_extensions.py
@@ -10,7 +10,7 @@ from ansible.module_utils.basic import AnsibleModule
 __metaclass__ = type
 
 
-def is_extension_installed(module, executable, name):
+def is_extension_installed(module, executable, name, dirname):
     rc, stdout, stderr = module.run_command(
         [executable, '--list-extensions', name])
     if rc != 0 or stderr:
@@ -23,26 +23,27 @@ def is_extension_installed(module, executable, name):
     return match is not None
 
 
-def list_extension_dirs(executable):
-    dirname = '.vscode'
-    if executable == 'code-insiders':
-        dirname += '-insiders'
-    if executable == 'code-oss':
-        dirname += '-oss'
-
+def list_extension_dirs(module, executable, dirname):
     ext_dir = os.path.expanduser(
         os.path.join('~', dirname, 'extensions'))
+
+    if not os.path.exists(ext_dir):
+        # Some builds (e.g. antigravity, cursor) do not create the
+        # extensions dir until after the first extension is installed.
+        # This is not an error; we will indicate that no extensions
+        # are installed. Also happens if the user deletes the directory
+        return []
 
     ext_dirs = sorted([f for f in os.listdir(
         ext_dir) if os.path.isdir(os.path.join(ext_dir, f))])
     return ext_dirs
 
 
-def install_extension(module, executable, name):
-    if is_extension_installed(module, executable, name):
+def install_extension(module, executable, name, dirname):
+    if is_extension_installed(module, executable, name, dirname):
         # Use the fact that extension directories names contain the version
         # number
-        before_ext_dirs = list_extension_dirs(executable)
+        before_ext_dirs = list_extension_dirs(module, executable, dirname)
         # Unfortunately `--force` suppresses errors (such as extension not
         # found)
         rc, stdout, stderr = module.run_command(
@@ -53,7 +54,7 @@ def install_extension(module, executable, name):
             module.fail_json(
                 msg=(f'Error while upgrading extension [{name}]: '
                      f'({rc}) {stdout + stderr}'))
-        after_ext_dirs = list_extension_dirs(executable)
+        after_ext_dirs = list_extension_dirs(module, executable, dirname)
         changed = before_ext_dirs != after_ext_dirs
         return changed, 'upgrade'
     else:
@@ -69,8 +70,8 @@ def install_extension(module, executable, name):
         return changed, 'install'
 
 
-def uninstall_extension(module, executable, name):
-    if is_extension_installed(module, executable, name):
+def uninstall_extension(module, executable, name, dirname):
+    if is_extension_installed(module, executable, name, dirname):
         rc, stdout, stderr = module.run_command(
             [executable, '--uninstall-extension', name])
         if 'successfully uninstalled' not in (stdout + stderr):
@@ -86,12 +87,7 @@ def run_module():
     module_args = dict(
         executable=dict(
             type='str',
-            required=False,
-            choices=[
-                'code',
-                'code-insiders',
-                'code-oss'],
-            default='code'),
+            required=True),
         name=dict(
             type='str',
             required=True),
@@ -100,27 +96,28 @@ def run_module():
             default='present',
             choices=[
                 'absent',
-                'present']))
+                'present']),
+        dirname=dict(
+            type='str',
+            required=True))
 
     module = AnsibleModule(argument_spec=module_args,
                            supports_check_mode=False)
 
     executable = module.params['executable']
-    if executable != 'code-insiders' and executable != 'code-oss':
-        executable = 'code'
-
     name = module.params['name']
     state = module.params['state']
+    dirname = module.params['dirname']
 
     if state == 'absent':
-        changed = uninstall_extension(module, executable, name)
+        changed = uninstall_extension(module, executable, name, dirname)
 
         if changed:
             msg = f'{name} is now uninstalled'
         else:
             msg = f'{name} is not installed'
     else:
-        changed, change = install_extension(module, executable, name)
+        changed, change = install_extension(module, executable, name, dirname)
 
         if changed:
             if change == 'upgrade':
